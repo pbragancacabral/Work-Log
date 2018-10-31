acf11df4f7aaa4a62300040a51fb2acf9332c98b
Pedro de Bragança Cabral <pbragancacabral@gmail.com>
2018-10-31 20:22:17 +0100

    Add tasks and find them by date

--- work_log.py
+++ work_log.py
@@ -1,4 +1,10 @@
+import csv
+import datetime
 import os
+
+from task import Task
+
+FILE = "log.csv"
 
 
 def clear_screen():
@@ -19,10 +25,12 @@ def show_main_menu():
 def show_entry_menu():
     clear_screen()
     date = input("Date of the task (DD/MM/YYYY): ")
-    task = input("Title of the task: ")
+    title = input("Title of the task: ")
     time = input("Time spent in (minutes): ")
     notes = input("Notes (optional): ")
-    return date, task, time, notes
+    fmt = "%d/%m/%Y"
+    date = datetime.datetime.strptime(date, fmt).date()
+    return Task(date, title, time, notes)
 
 
 def show_entries_menu():
@@ -37,24 +45,42 @@ def show_entries_menu():
 
 
 def run():
-    while True:
-        choice = show_main_menu()
+    choice = show_main_menu()
+    if choice == "1":
+        entry = show_entry_menu()
+        with open(FILE, "a") as csvfile:
+            fieldnames = ["date", "title", "minutes", "notes"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            # writer.writeheader()
+            writer.writerow({
+                "date": entry.date,
+                "title": entry.title,
+                "minutes": entry.minutes,
+                "notes": entry.notes
+            })
+        input("Entry added - please press <RETURN> to return to the main menu.")
+        show_main_menu()
+    elif choice == "2":
+        choice = show_entries_menu()
         if choice == "1":
-            entry = show_entry_menu()
-            # save entry
-            clear_screen()
-            input("Entry added - please press <RETURN> to return to the main menu.")
+            date_to_search = input("Enter the date to search with format dd/mm/yyyy: ")
+            day, month, year = date_to_search.split("/")
+            fmt_date_to_search = f"{year}-{month}-{day}"
+            with open(FILE, newline='') as csvfile:
+                reader = csv.DictReader(csvfile, delimiter=",")
+                rows = list(reader)
+                for row in rows[1:]:
+                    if row["date"] == fmt_date_to_search:
+                        print(row)
+
+        if choice == "5":
             show_main_menu()
-        elif choice == "2":
-            choice = show_entries_menu()
-            if choice == "5":
-                show_main_menu()
-        elif choice == "3":
-            print("Quitting...")
+    elif choice == "3":
+        print("Quitting...")
         exit()
     else:
-        print("Error: please select a number from the menu.")
-        print()
+        input("\nPlease select a number from the menu.\nPress <enter> to continue.")
+        run()
 
 
 if __name__ == "__main__":
